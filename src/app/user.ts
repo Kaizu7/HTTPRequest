946785efd4d8f68137a593d48d7035072d145f89
Kaizad Beddingwala <59142167+Kaizu7@users.noreply.github.com>
2021-10-06 17:34:29 +0530

    form-making

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public EmployeeId: String,
+        public FirstName: String,
+        public LastName: String,
+        public Department: String,
+        public Description: String
+    ) {}
+}
